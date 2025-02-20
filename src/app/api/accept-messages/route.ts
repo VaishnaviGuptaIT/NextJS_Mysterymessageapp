d53def1de10306501b53dd24e899194292b01b26
Vaishnavi Gupta <vaishurgupta2010@gmail.com>
2025-02-20 16:50:05 +0530

    backend code done

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -89,7 +89,7 @@ export async function GET(request: Request) {
     return Response.json(
       {
         success: true,
-        isAcceptingMessages: foundUser.isAcceptingMessages,
+        isAcceptingMessages: foundUser.isAcceptingMessage,
       },
       { status: 200 }
     );
